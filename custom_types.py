f379a4ba6fe9e94d42e4da0d96c48f0d2e4ce61b
Rohan Sapre <rohansapre@ccs.neu.edu>
2016-10-21 04:21:03 -0400

    created dataset for user book and ratings

--- custom_types.py
+++ custom_types.py
@@ -6,10 +6,20 @@ class User(object):
 		self.location = location
 		self.age = age
 
+	def getID():
+		self.id
+
 class Book(object):
 	"""docstring for Book"""
 	def __init__(self, isbn, title):
 		super(Book, self).__init__()
 		self.isbn = isbn
 		self.title = title
-		
+
+class Rating(object):
+	"""docstring for Rating"""
+	def __init__(self, user_id, isbn, value):
+		super(Rating, self).__init__()
+		self.user_id = user_id
+		self.isbn = isbn
+		self.value = value
--- recommend.py
+++ recommend.py
@@ -12,7 +12,7 @@ y(i, j) = rating given by user j to movie i
 
 # import numpy as np
 import csv
-import customer_types
+import custom_types
 
 # users = np.loadtxt("data/BX-Users.csv", delimiter=';', skiprows=1)
 users = []
@@ -20,7 +20,26 @@ with open('data/BX-Users.csv', 'rb') as user_file:
 	reader = csv.reader(user_file, delimiter=';')
 	reader.next()
 	for row in reader:
-		users.append(user.User(int(row[0]), str(row[1]), str(row[2])))
-# books = np.loadtxt("data/BX-Books.csv")
-# book-ratings = np.loadtxt("data/BX-Book-Ratings.csv")
+		users.append(custom_types.User(int(row[0]), str(row[1]), str(row[2])))
 
+# books = np.loadtxt("data/BX-Books.csv")
+books = []
+with open('data/BX-Books.csv', 'rb') as book_file:
+	reader = csv.reader(book_file, delimiter=';')
+	reader.next()
+	for row in reader:
+		books.append(custom_types.Book(str(row[0]), str(row[1])))
+
+# book-ratings = np.loadtxt("data/BX-Book-Ratings.csv")
+ratings = {}
+with open('data/BX-Book-Ratings.csv', 'rb') as rating_file:
+	reader = csv.reader(rating_file, delimiter=';')
+	reader.next()
+	for row in reader:
+		ratings[(int(row[0]), str(row[1]))] = custom_types.Rating(int(row[0]), str(row[1]), int(row[2]))
+		# ratings.append(custom_types.Rating(int(row[0]), str(row[1]), int(row[2])))
+
+rating_matrix = []
+for book in books:
+	for user in users:
+		if ()
